dedb99810833291251902af515eff4b477e2e1c8
Hazimzaky <hazemnzaky@gmail.com>
2025-09-20 08:38:39 +0300

    Delete dist directory

--- dist/models/Product.d.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-import { IProduct } from '../types';
-declare const _default: mongoose.Model<IProduct & mongoose.Document<unknown, any, any, Record<string, any>, {}>, {}, {}, {}, mongoose.Document<unknown, {}, IProduct & mongoose.Document<unknown, any, any, Record<string, any>, {}>, {}, {}> & IProduct & mongoose.Document<unknown, any, any, Record<string, any>, {}> & Required<{
-    _id: string;
-}> & {
-    __v: number;
-}, any>;
-export default _default;
-//# sourceMappingURL=Product.d.ts.map
--- dist/utils/validation.d.ts
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-import { Request, Response, NextFunction } from 'express';
-export declare const productSchema: Joi.ObjectSchema<any>;
-export declare const querySchema: Joi.ObjectSchema<any>;
-export declare const idSchema: Joi.ObjectSchema<any>;
-export declare const validate: (schema: Joi.ObjectSchema, property?: "body" | "query" | "params") => (req: Request, res: Response, next: NextFunction) => void;
-export declare const productUpdateSchema: Joi.ObjectSchema<any>;
-export declare const bulkOperationSchema: Joi.ObjectSchema<any>;
-//# sourceMappingURL=validation.d.ts.map
