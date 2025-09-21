e32c93c0e57f19e6022b08f9f2992a9adc26f598
Hazimzaky <hazemnzaky@gmail.com>
2025-09-22 02:52:25 +0300

    Add files via upload

--- src/scripts/seedData.ts
+++ src/scripts/seedData.ts
@@ -53,7 +53,7 @@ const sampleProducts: Partial<IProduct>[] = [
     name: { en: 'Orange', ar: 'برتقال' },
     category: 'Fruits',
     price: { value: 3.49, currency: 'USD', unit: 'kg' },
-    images: ['https://images.unsplash.com/photo-1557800634-7bf3c73cec63?w=500&fm=jpg'],
+    images: ['https://images.unsplash.com/photo-1580052614034-c55d20bfee3b?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'],
     description: 'Juicy, vitamin C-rich oranges perfect for fresh juice or eating fresh.',
     attributes: {
       organic: true,
@@ -779,7 +779,7 @@ const sampleProducts: Partial<IProduct>[] = [
     name: { en: 'Asparagus', ar: 'هليون' },
     category: 'Vegetables',
     price: { value: 6.99, currency: 'USD', unit: 'bunch' },
-    images: ['https://plus.unsplash.com/premium_photo-1722988820957-cc22d1214e50?q=80&w=804&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'],
+    images: ['https://images.unsplash.com/photo-1595855759920-86582396756a?q=80&w=464&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'],
     description: 'Fresh asparagus perfect for grilling, roasting, or steaming. Rich in folate and vitamin K.',
     attributes: {
       organic: true,
