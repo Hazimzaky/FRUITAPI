d4fe28d7b28aef30ce5f0d5daf703428ddcf96d3
Hazimzaky <hazemnzaky@gmail.com>
2025-09-22 02:13:48 +0300

    Delete dist directory

--- dist/middleware/rateLimiter.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const apiLimiter: import("express-rate-limit").RateLimitRequestHandler;
-export declare const strictLimiter: import("express-rate-limit").RateLimitRequestHandler;
-export declare const createProductLimiter: import("express-rate-limit").RateLimitRequestHandler;
-export declare const searchLimiter: import("express-rate-limit").RateLimitRequestHandler;
-//# sourceMappingURL=rateLimiter.d.ts.map
--- dist/scripts/seedData.d.ts
+++
@@ -1,5 +0,0 @@
-import { IProduct } from '../types';
-declare const sampleProducts: Partial<IProduct>[];
-declare function seedDatabase(): Promise<void>;
-export { seedDatabase, sampleProducts };
-//# sourceMappingURL=seedData.d.ts.map
